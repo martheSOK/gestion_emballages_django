31ec62f9800e6e51b4ee5f098727ee8e66b86741
bakiscofield <bakitchassei@gmail.com>
2024-01-31 16:37:12 +0000

    jkgvhk

--- gestion_emballage/gestion_emballage/settings.py
+++ gestion_emballage/gestion_emballage/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'afrikpros',
+    'django_browser_reload',
 ]
 
 MIDDLEWARE = [
