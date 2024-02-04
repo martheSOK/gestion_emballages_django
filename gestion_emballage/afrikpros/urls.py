9eb60aac4988dda502d06c967cabf60aa4d56446
bakiscofield <bakitchassei@gmail.com>
2024-02-04 21:22:20 +0000

    crud depot ok

--- gestion_emballage/afrikpros/urls.py
+++ gestion_emballage/afrikpros/urls.py
@@ -7,6 +7,13 @@ app_name='afrikpros'
 
 urlpatterns = [
        path('', views.dashboard, name='dashboard'),
-       path('add_depot/', views.add_depot, name='add_depot'),
-       # path('liste_depot/', views.liste_depot, name='liste_depot'),
-]
+       # path('liste_depot/', views.liste_depot, name='add_depot'),
+#        path('update_depot/<int:depot_id>/', views.update_depot, name='update_depot'),
+       path('liste_depot/', views.liste_depot, name='liste_depot'),
+
+    # URL for updating an existing depot
+    path('liste_depot/<int:depot_id>/',  views.liste_depot, name='update_depot'),
+    path('delete_depot/<int:depot_id>/',  views.delete_depot, name='delete_depot'),
+ ]
+
+
--- gestion_emballage/afrikpros/views.py
+++ gestion_emballage/afrikpros/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
+
 # Create your views here.
 
 def dashboard(request):
@@ -7,20 +8,44 @@ def dashboard(request):
 
 
 
-def add_depot(request):
-    print("::::::::::add_depot:::::::::::::::::::")
-    if request.method == "POST":
-        form = DepotForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("afrikpros:liste_depot")
-    else:
-        
-        form = DepotForm()
-    print(form)
-    return render(request, "depot/add_depot.html", locals()) 
-   
+
    
 def liste_depot(request):
     listedepot=Depot.objects.all()
     return render( request,"depot/liste_depot.html",locals())
+
+
+
+
+
+
+def liste_depot(request, depot_id=None):
+    if depot_id:
+        # Update existing depot
+        depot = get_object_or_404(Depot, pk=depot_id)
+        if request.method == "POST":
+            form = DepotForm(request.POST, instance=depot)
+            if form.is_valid():
+                form.save()
+                return redirect("afrikpros:liste_depot")
+        else:
+            form = DepotForm(instance=depot)
+    else:
+        # Add new depot
+        if request.method == "POST":
+            form = DepotForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect("afrikpros:liste_depot")
+        else:
+            form = DepotForm()
+
+    listedepot = Depot.objects.all()
+    return render(request, "depot/liste_depot.html", {"form": form, "listedepot": listedepot})
+
+def delete_depot(request, depot_id):
+    depot = Depot.objects.get(pk=depot_id)
+    depot.delete()    
+    return redirect("afrikpros:liste_depot")
+
+
